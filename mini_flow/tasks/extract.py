b052c1047b231809e5d9efd43e7394592c0c4928
FernandoCelmer <email@fernandocelmer.com>
2025-03-21 00:48:14 -0300

    ⚙️ FEATURE: Examples

--- mini_flow/tasks/extract.py
+++
@@ -1,8 +0,0 @@
-from dotflow import action
-
-from requests import get  # type: ignore
-
-
-@action(retry=5)
-def extract(initial_context):
-    return get(initial_context.storage).text
--- mini_flow/tasks/load.py
+++
@@ -1,7 +0,0 @@
-from dotflow import action
-
-
-@action
-def load(previous_context):
-    with open(file="book.json", mode="w", encoding="utf-8") as file:
-        file.write(previous_context.storage[2].storage)
