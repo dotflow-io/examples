fedfba6d7e1aafa7bf8c2bb6ff3a3edf3db1257c
FernandoCelmer <email@fernandocelmer.com>
2025-05-04 04:46:30 -0300

    ⚙️ FEATURE: Updated

--- step_with_notify_telegram.py
+++ step_with_notify_telegram.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import time
 
 from dotenv import load_dotenv
 
 from dotflow import Config, DotFlow, action
-from dotflow.types import TypeStatus
 from dotflow.notify import NotifyTelegram
+from dotflow.types import TypeStatus
 
 
 @action
-def simple_step():
+def simple_step(initial_context):
+    time.sleep(0.5)
     return "ok"
+
+
+@action
+def simple_step_raise():
+    raise Exception("Fail!")
 
 
 def main():
@@ -20,12 +27,15 @@ def main():
     notify = NotifyTelegram(
         token=os.getenv("TOKEN"),
         chat_id=os.getenv("CHAT_ID"),
-        notification_type=TypeStatus.FAILED,
+        notification_type=TypeStatus.FAILED
     )
 
-    workflow = DotFlow(config=Config(notify=notify))
+    workflow = DotFlow(
+        config=Config(notify=notify)
+    )
 
-    workflow.task.add(step=simple_step)
+    workflow.task.add(step=simple_step, initial_context={"foo": "bar"})
+    workflow.task.add(step=simple_step_raise)
     workflow.start()
 
     return workflow
